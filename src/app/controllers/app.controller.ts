722bc9642ad819b6b8b6920cb71295d5c6c1b0ac
Thisara Senadeera <thisarasenadeera2000.com>
2025-04-23 13:43:09 +0530

    Fix 404 error by adding root route and adjusting global prefix for Vercel deployment

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -1,6 +1,7 @@
 import {
   Controller,
   Post,
+  Get, // Add Get decorator
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -11,11 +12,16 @@ import { AppService } from '../services/app.service';
 export class AppController {
   constructor(private appService: AppService) {}
 
+  // Add this root route for GET /
+  @Get('/')
+  getRoot(): string {
+    return 'Nest.js backend is running!';
+  }
+
   @Post('upload')
   @UseInterceptors(FileInterceptor('image'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     const response = await this.appService.uploadImageToCloudinary(file);
-
     return response.url;
   }
 }
