6acbf46c451cbebe6bc239222511b879cfcbeccc
Thisara Senadeera <thisarasenadeera2000@gmail.com>
2025-05-11 01:02:47 +0530

    Revert "Merge pull request #1 from 2000Thisara/orderModule"
    This reverts commit a7b9732febd6462b6ef29b632f9f13a151ee205e, reversing
    changes made to 38bf1994b5ec8a95728fe2c9287bdb3772ae89c8.

--- src/orders/services/orders.service.ts
+++ src/orders/services/orders.service.ts
@@ -1,15 +1,23 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
-import { PaymentResultDto } from '../dto/payment-result.dto';
-import { CreateOrderDto } from '../dto/create-order.dto';
+import { PaymentResult } from 'src/interfaces';
 import { Order, OrderDocument } from '../schemas/order.schema';
 
 @Injectable()
 export class OrdersService {
-  constructor(@InjectModel(Order.name) private orderModel: Model<OrderDocument>) {}
+  constructor(
+    @InjectModel(Order.name) private orderModel: Model<OrderDocument>
+  ) {}
 
-  async create(createOrderDto: CreateOrderDto, userId: string): Promise<OrderDocument> {
+  async create(
+    orderAttrs: Partial<OrderDocument>,
+    userId: string
+  ): Promise<OrderDocument> {
     const {
       orderItems,
       shippingDetails,
@@ -18,7 +26,7 @@ export class OrdersService {
       taxPrice,
       shippingPrice,
       totalPrice,
-    } = createOrderDto;
+    } = orderAttrs;
 
     if (orderItems && orderItems.length < 1)
       throw new BadRequestException('No order items received.');
@@ -38,21 +46,28 @@ export class OrdersService {
   }
 
   async findAll(): Promise<OrderDocument[]> {
-    return this.orderModel.find();
+    const orders = await this.orderModel.find();
+
+    return orders;
   }
 
   async findById(id: string): Promise<OrderDocument> {
     if (!Types.ObjectId.isValid(id))
       throw new BadRequestException('Invalid order ID.');
 
-    const order = await this.orderModel.findById(id).populate('user', 'name email');
+    const order = await this.orderModel
+      .findById(id)
+      .populate('user', 'name email');
 
     if (!order) throw new NotFoundException('No order with given ID.');
 
     return order;
   }
 
-  async updatePaid(id: string, paymentResultDto: PaymentResultDto): Promise<OrderDocument> {
+  async updatePaid(
+    id: string,
+    paymentResult: PaymentResult
+  ): Promise<OrderDocument> {
     if (!Types.ObjectId.isValid(id))
       throw new BadRequestException('Invalid order ID.');
 
@@ -61,10 +76,12 @@ export class OrdersService {
     if (!order) throw new NotFoundException('No order with given ID.');
 
     order.isPaid = true;
-    order.paidAt = new Date();
-    order.paymentResult = paymentResultDto;
+    order.paidAt = Date();
+    order.paymentResult = paymentResult;
 
-    return order.save();
+    const updatedOrder = await order.save();
+
+    return updatedOrder;
   }
 
   async updateDelivered(id: string): Promise<OrderDocument> {
@@ -76,13 +93,16 @@ export class OrdersService {
     if (!order) throw new NotFoundException('No order with given ID.');
 
     order.isDelivered = true;
-    order.deliveredAt = new Date();
+    order.deliveredAt = Date();
 
-    return order.save();
+    const updatedOrder = await order.save();
+
+    return updatedOrder;
   }
 
   async findUserOrders(userId: string) {
-    return this.orderModel.find({ user: userId });
+    const orders = await this.orderModel.find({ user: userId });
+
+    return orders;
   }
 }
-
