2577beed06910cbc40b7f95b67934f550fe27410
Thisara Senadeera <thisarasenadeera2000@gmail.com>
2025-05-20 12:16:30 +0530

    Update products

--- src/products/dtos/review.dto.ts
+++ src/products/dtos/review.dto.ts
@@ -1,11 +1,12 @@
 import { IsString, IsNumber, Min, Max } from 'class-validator';
 
+// Data Transfer Object for review data validation and typing
 export class ReviewDto {
-  @Min(1)
-  @Max(5)
-  @IsNumber()
+  @Min(1) // Ensures rating is at least 1
+  @Max(5) // Ensures rating does not exceed 5
+  @IsNumber() // Validates that 'rating' is a number
   rating: number;
 
-  @IsString()
+  @IsString() // Validates that 'comment' is a string
   comment: string;
 }
