a83e0be862e8a3e1e87494765e49b8aaf9284f15
vihangarasanjali <rvihanga0506@gmail.com>
2025-07-20 22:44:26 +0530

    Payemnt Gateway Completed

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -17,8 +17,11 @@ export class PaymentService {
         payment_method_types: ['card'],
         line_items: lineItems,
         mode: 'payment',
-        success_url: `${process.env.CLIENT_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+        success_url: `${process.env.CLIENT_URL}/successPayment?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.CLIENT_URL}/cancel`,
+        shipping_address_collection: {
+          allowed_countries: ['LK', 'US', 'CA'], // ✅ Add countries here
+        },
       });
       return { id: session.id, url: session.url };
     } catch (error) {
