9388bc42cb43ec0984f97b5203a537b96051a8e6
Nithakshi Dishara <nithakshidishara2002@gmail.com>
2025-07-21 19:49:49 +0530

    updated Privacy & Policy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 import { AppService } from './services/app.service';
 import { ServicesModule } from 'src/services/services.module';
 import { privacyPolicyModule } from 'src/privacyPolicy/privacyPolicy.module';
+import { BranchesModule } from 'src/branches/branches.module';
+import { BannerModule } from 'src/banner/banner.module';
 
 @Module({
   imports: [
